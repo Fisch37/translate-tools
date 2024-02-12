b6f21defd09290362028ae099c15b324732caaf9
Fisch37 <seracin4@gmail.com>
2024-02-12 19:15:32 +0100

    Added support for directory translation

--- console.py
+++ console.py
@@ -1,6 +1,10 @@
-from argparse import ArgumentParser, FileType
+from argparse import ArgumentParser
+from io import TextIOWrapper
+from itertools import repeat
 from os import environ
-from sys import exit, stderr
+from pathlib import Path
+from sys import exit, stdin, stderr
+from typing import Iterable
 
 from ext_api.helpers import ProgressCounter, ensure_language
 from ext_api.translate import get_random_sequence
@@ -34,16 +38,16 @@ parser.add_argument(
     help="The source file to read query from. If left unset, data is read from stdin.\
         Note that using this as an interactive application you will have to feed EOF \
         using Ctrl+D (on POSIX) or Ctrl+Z + Enter (on Windows)",
-    type=FileType("r", encoding="utf-8"),
+    type=Path,
     required=False,
-    default="-",
+    default=None,
 )
 parser.add_argument(
     "--output", "-o",
     help="The file to output the translation into. If left unset, outputs to stdout instead",
-    type=FileType("w", encoding="utf-8"),
+    type=Path,
     required=False,
-    default="-"
+    default=None
 )
 parser.add_argument(
     "--silent", "-s",
@@ -100,8 +104,27 @@ jamble_translate.add_argument(
     action="store_true"
 )
 
+
 def progress_callback(progress: ProgressCounter):
     print(f"\r{progress.state}/{progress.end}", end="", flush=True)
+
+def iterate_through_files(path: Path):
+    for subpath in filter(lambda p: p.is_file(), path.iterdir()):
+        with subpath.open("r", encoding="utf-8") as file:
+            yield file.read()
+
+def iterate_new_file_objects(destination: Path, pattern: Path):
+    for p in pattern.iterdir():
+        path = destination / p.name
+        file = path.open("w", encoding="utf-8")
+        yield file
+        file.close()
+
+def repeat_file_object(source: Path):
+    while True:
+        with source.open("a", encoding="utf-8") as file:
+            yield file
+            file.write("\n\n")
 
 def main() -> int:
     namespace = parser.parse_args()
@@ -109,12 +132,11 @@ def main() -> int:
         environ["ARGOS_DEVICE_TYPE"] = "cuda"
     segment_length: int = namespace.segment_size
     pool_size: int|None = namespace.pool_size
-    with namespace.input as file:
-        query: str = file.read()
-    output = namespace.output
+    input_path: Path|None = namespace.input
+    output_path: Path|None = namespace.output
     silent: int = namespace.silent
     
-    
+    # Generate translation sequence
     if namespace.subparser == "translate":
         try:
             sequence = [
@@ -147,14 +169,39 @@ def main() -> int:
         print(NotImplementedError("Unknown action"), file=stderr)
         return 100
     
-    result = translate_sequence_segmented(
-        query,
-        sequence,
-        max_segment_size=segment_length,
-        pool_size=pool_size,
-        callback=progress_callback if silent == 0 else None
-    )
-    print(result, file=output)
+    # Setup IO
+    queries: Iterable[str]
+    has_multiple_inputs = False
+    if input_path is None:
+        queries = (stdin.read(), )
+    elif input_path.is_dir():
+        queries = iterate_through_files(input_path)
+        has_multiple_inputs = True
+    else:
+        with input_path.open("r", encoding="utf-8") as file:
+            queries = (file.read(), )
+
+    outputs: Iterable[TextIOWrapper|None]
+    if output_path is None:
+        outputs = repeat(None)
+    elif output_path.is_dir():
+        if not has_multiple_inputs:
+            print("Cannot write single-source output to directory", file=stderr)
+            return 104
+        outputs = iterate_new_file_objects(output_path, input_path)
+    else:
+        outputs = repeat_file_object(output_path)
+
+    # Translate
+    for query, output in zip(queries, outputs):
+        result = translate_sequence_segmented(
+            query,
+            sequence,
+            max_segment_size=segment_length,
+            pool_size=pool_size,
+            callback=progress_callback if silent == 0 else None
+        )
+        print(result, file=output)
     return 0
 
 if __name__ == "__main__":
